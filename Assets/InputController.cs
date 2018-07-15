8f742010672f6a2b74475439812475b1c0d71cfa
ChoiDG <41226537+ChoiDG@users.noreply.github.com>
2018-07-15 23:49:38 +0900

    tt2

--- Assets/InputController.cs
+++ Assets/InputController.cs
@@ -15,39 +15,37 @@ public class InputController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        var clickStream = Observable.EveryUpdate()
-            .Select(_ => Input.GetMouseButtonDown(0));
+#if UNITY_EDITOR
+        var drag = Observable.EveryUpdate().Select(pos => Input.mousePosition);
+        var stop = Observable.EveryUpdate().Where(_ => Input.GetMouseButtonUp(0)).First();
+        var start = Observable.EveryUpdate().Where(_ => Input.GetMouseButtonDown(0)).First();
+#elif UNITY_IOS || UNITY_ANDROID
+    var drag = Observable.EveryUpdate ().Select (pos => Input.GetTouch(0).position);
+    var stop = Observable.EveryUpdate ().Where(_ => Input.touchCount != 1).First();
+#endif
 
-        var clickUpStream = Observable.EveryUpdate()
-            .Where(_ => Input.GetMouseButtonUp(0));
-
-        var dragStream = Observable.EveryUpdate()
-            .SkipUntil(clickStream)
-            .Select(_ => new Vector3(Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y"))) // 변화량
-            .TakeUntil(clickUpStream)
+        // 행동을 먼저 구현하고 받아야할 데이터를 가공한다.!!
+        IDisposable onDrag = drag.Buffer(3)
+            .SkipUntil(start)
+            .TakeUntil(stop)
             .Repeat()
-            .Subscribe(_ =>
-            {
-#if _DEBUG_
-                Debug.Log(string.Format("Drag : {0}", _));
-#endif
-                if (_DragEventHandler == null)
-                    return;
-
-                _DragEventHandler(_);
+            .Subscribe(colPos => {
+                float delPosx = Input.mousePosition.x - colPos.First().x;
+                float delPosz = Input.mousePosition.z - colPos.First().y;
+                Vector3 vec = new Vector3(delPosx, 0, delPosz);
+                Debug.Log(vec);
             });
+        //        clickStream.Buffer(clickStream.Throttle(TimeSpan.FromMilliseconds(250)))
+        //            .Where(xs => xs.Count >= 2)
+        //            .Subscribe(xs =>
+        //            {
+        //#if _DEBUG_
+        //                Debug.Log("Double_Click");
+        //#endif
+        //                if (_DoubleClickEventHandler == null)
+        //                    return;
 
-        clickStream.Buffer(clickStream.Throttle(TimeSpan.FromMilliseconds(250)))
-            .Where(xs => xs.Count >= 2)
-            .Subscribe(xs =>
-            {
-#if _DEBUG_
-                Debug.Log("Double_Click");
-#endif
-                if (_DoubleClickEventHandler == null)
-                    return;
-
-                _DoubleClickEventHandler();
-            });
+        //                _DoubleClickEventHandler();
+        //            });
     }
 }
